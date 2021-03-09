42a2dae9fd7d993c3e272e4284f86c98d579b78b
simopek <>
2021-03-09 23:29:48 +0100

    Add roll and count up buttons

--- diceroller/app/src/main/java/com/simopek/diceroller/MainActivity.kt
+++ diceroller/app/src/main/java/com/simopek/diceroller/MainActivity.kt
@@ -2,10 +2,36 @@ package com.simopek.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener { rollDice() }
+
+        val countUpButton: Button = findViewById(R.id.count_up_button)
+        countUpButton.setOnClickListener { countUp() }
+    }
+
+    private fun getRollResultTextView(): TextView = findViewById(R.id.roll_result_text)
+
+    private fun countUp() {
+
+        val rollResult = getRollResultTextView().text
+
+        val rollResultAsInt = rollResult.toString().toIntOrNull() ?: return
+
+        getRollResultTextView().text = (if (rollResultAsInt >= 6) 6 else rollResultAsInt + 1).toString()
+    }
+
+    private fun rollDice() {
+
+        val randomNumber = (1..6).random()
+
+        getRollResultTextView().text = randomNumber.toString()
     }
 }
